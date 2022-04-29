df7db262fe9bbf6d3b6ff4dd5c84b3f2c80a12ad
robby <robin.kw99@yahoo.com>
2022-04-29 15:40:07 +0530

    Alert pop-up created

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 // function to clear form
-const alertDiv = document.getElementById(`alert`);
+const alertDiv = document.getElementById(`alertDiv`);
 
 function displayAlert() {
-	const clearForm = document.getElementById(`clearForm`);
+	let clearForm = document.getElementById(`clearForm`);
 	clearForm.addEventListener('click', () => {
 		alertDiv.style.display = `flex`;
 	});
 }
 
 displayAlert();
+
+function hideAlert() {
+	alertDiv.style.display = `none`;
+}
+
+// function on pressing cancel btn
+
+function onCancel() {
+	let cancelBtn = document.getElementById(`cancelBtn`);
+	cancelBtn.addEventListener('click', () => hideAlert());
+}
+
+onCancel();
+
+// function on pressing clear btn
+
+function onClear() {
+	let clearBtn = document.getElementById(`clearBtn`);
+	clearBtn.addEventListener('click', () => {
+		let googleForm = document.getElementById(`googleForm`);
+		googleForm.reset();
+		hideAlert();
+	});
+}
+
+onClear();
